f04bead7d8d112a40a5a6da4530997aa269feb0f
Lyr-7D1h <lyr-7d1h@pm.me>
2021-11-29 19:19:40 +0100

    optimized higly divislbe triangular number

--- highly_divisible_triangular_number.py
+++ highly_divisible_triangular_number.py
@@ -1,5 +1,49 @@
+def is_prime(x: int):
+    """6k+-1 Primality test optimized"""
+    if x % 3 == 0:
+        return False
+    i = 5
+    while i <= (x ** 0.5) + 1:
+        if x % i == 0 or x % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
 
+primes = [2, 3]
+
+
+# 20 function calls in 0.027 seconds
+# https://en.wikipedia.org/wiki/Divisor_function#Properties
+def using_primes(divisible_limit):
+    divisible_count = 0
+    x = 1
+
+    # Generate primes till max prime divisor
+    for check_prime in range(primes[-1] + 2, int(divisible_limit ** 0.5), 2):
+        if is_prime(check_prime):
+            primes.append(check_prime)
+
+    while divisible_count < divisible_limit:
+        divisible_count = 1
+        x += 1
+        triangular = int(x * (x + 1) / 2)
+
+        temp = triangular
+        for prime in primes:
+            if temp % prime == 0:
+                c = 0
+                while temp % prime == 0:
+                    c += 1
+                    temp = temp / prime
+                divisible_count *= (c + 1)
+                if temp == 1:
+                    break
+
+    return triangular
+
+
+# 5 function calls in 2.822 seconds
 def naive(divisble_limit):
     divisible_count = 0
     x = 1
@@ -10,15 +54,13 @@ def naive(divisble_limit):
 
         for i in range(1, int(t ** 0.5) + 1):
             if t % i == 0:
-                # divisible_count += 1
-                # If divisors are equal,
-                # count only one
+                # If divisors are equal only count 1
                 if (t / i == i):
                     divisible_count += 1
-                else:  # Otherwise count both
+                else:  # Otherwise count divisor pair
                     divisible_count += 2
-        print(t, divisible_count)
     return t
 
 
-print(naive(5))
+# print(naive(500))
+print(using_primes(500))
