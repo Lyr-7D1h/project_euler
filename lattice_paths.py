5b1106af7c12ca1e119f1ee0bf4ad96469dea4c4
lyr <lyr-7d1h@pm.me>
2022-01-05 19:27:51 +0100

    Finished lattice paths

--- lattice_paths.py
+++ lattice_paths.py
@@ -1,47 +1,37 @@
-def build_grid(size):
-    grid = []
-
-    for y in range(size + 1):
-        grid.append([])
-        for x in range(size + 1):
-            if y == size:
-                grid[y].append([False, True])
-            elif x == size:
-                grid[y].append([True, False])
-            else:
-                grid[y].append([True, True])
-
-    return grid
-
-
-def has_path(grid, y, x):
-    grid_size = len(grid) - 1
-
-    # Is at the end?
-    if x == grid_size and y == grid_size:
-        return True
-
-    # Go right if possible
-    if grid[y][x][1]:
-        grid[y][x][1] = False
-        return has_path(grid, y, x + 1)
-
-    # Go down if possible
-    if grid[y][x][0]:
-        grid[y][x][0] = False
-        return has_path(grid, y + 1, x)
-
-    return False
+#
+# Basically Pascals Triangle in a grid form: https://en.wikipedia.org/wiki/Pascal%27s_triangle
+#
+# 2 x 2 Block Grid:
+#
+# 6 3 1
+# 3 2 1
+# 1 1 1
+#
+# 1
+# 1 1
+# 1 2 1
+# 1 3 3 1
+# 1 4 6 4 1
 
 
-def count_pathways(grid):
-    c = 0
-    while has_path(grid, 0, 0):
-        c += 1
-    return c
+# Create a pascal triangle
+def create_pascal_triangle(size):
+    triangle = [[1]]
+    for n in range(2, size + 1):
+        triangle.append([])
+
+        for i in range(n):
+            prev = triangle[n - 2]
+            x = prev[i - 1] if i - 1 >= 0 else 0
+            y = prev[i] if i < len(prev) else 0
+            triangle[n - 1].append(x + y)
+    return triangle
 
 
-grid = build_grid(2)
-print(count_pathways(grid))
-for x in grid:
-    print(x)
+def lattic_paths(grid_size):
+    last = create_pascal_triangle(grid_size * 2)[-1]
+    return last[len(last) // 2 - 1] + last[len(last) // 2]
+
+
+# 1685 function calls in 0.000 seconds
+print(lattic_paths(20))
