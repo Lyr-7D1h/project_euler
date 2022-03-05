d7e46fc0548155eda21e14f75c9a710fa03c3acf
lyr <lyr-7d1h@pm.me>
2022-03-05 15:48:50 +0100

    solved counting sundays

--- counting_sundays.py
+++ counting_sundays.py
@@ -14,19 +14,17 @@ months = [
 ]
 
 sundays = 0
-start_day = 0  # Start on monday
+start_day = 1  # Start on monday
 
-for year in range(1900, 2001):
+for year in range(1901, 2001):
+    months[1] = 28
     if year % 4 == 0 and not (year % 100 == 0 and year % 400):
         months[1] = 29
 
     for month, days in enumerate(months):
-        if month == 0:
-            sundays += (start_day + days) // 7
-            print(
-                f"{year} {month} starting on {start_day} has ", (start_day + days) // 7
-            )
-        start_day = days % 7
+        if start_day == 6:
+            sundays += 1
+        start_day = (days + start_day) % 7
 
 
 print(sundays)
