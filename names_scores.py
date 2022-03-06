a7a173ea9fa431bf550498309555fdf64f151db1
lyr <lyr-7d1h@pm.me>
2022-03-06 13:08:39 +0100

    started on names_scores

---
+++ names_scores.py
@@ -0,0 +1,13 @@
+def partition(left):
+    # TODO try with rand
+    # pivot = randrange(len(list))
+    pivot = left
+
+
+def quick_sort(list):
+    partition(0)
+
+
+with open('data/names.txt') as f:
+    names = f.read().replace("\"", "").split(",")
+    print(names)
