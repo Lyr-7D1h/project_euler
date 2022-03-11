b2ae0ea16ace57909b586f217b7ede2f0813b717
lyr <lyr-7d1h@pm.me>
2022-03-11 15:15:17 +0100

    names_scores solve

--- names_scores.py
+++ names_scores.py
@@ -1,13 +1,13 @@
-def partition(left):
-    # TODO try with rand
-    # pivot = randrange(len(list))
-    pivot = left
+# 30975 function calls in 0.009 seconds
 
+total = 0
+with open("data/names.txt") as f:
+    names = f.read().replace('"', "").split(",")
+    names.sort()
+    for i, name in enumerate(names):
+        val = 0
+        for c in name:
+            val += ord(c) - 64
+        total += val * (i + 1)
 
-def quick_sort(list):
-    partition(0)
-
-
-with open('data/names.txt') as f:
-    names = f.read().replace("\"", "").split(",")
-    print(names)
+print(total)
